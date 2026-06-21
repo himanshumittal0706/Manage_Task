03580a5fba39e3a0e2d2912f4e2a36431909e741
Himanshu Mittal <himanshumittal0706@gmail.com>
2026-06-21 23:34:28 +0530

    Some minor changes

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 
 export const NotFound = () => {
@@ -5,9 +6,12 @@ export const NotFound = () => {
         <div style={{ textAlign: "center", marginTop: "50px" }}>
             <h1>404</h1>
             <p>Page not found</p>
+
+            <Link to={"/"} className="">
+                Back to home page
+            </Link>
         </div>
     )
 }
-
 
 
